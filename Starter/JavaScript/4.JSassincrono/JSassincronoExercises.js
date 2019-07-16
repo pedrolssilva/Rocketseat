9e07df832af47ce0f234a2d91afeeeb86ca87032
pedrolssilva <pedrolssilva@hotmail.com>
2019-07-15 22:00:10 -0300

    Finalizando ex3 de renderizar loading o ao buscar info e informar erro ao nao encontrar os repositorios

--- Starter/JavaScript/4.JSassincrono/JSassincronoExercises.js
+++ Starter/JavaScript/4.JSassincrono/JSassincronoExercises.js
@@ -18,22 +18,41 @@ checkAge(15)
 //Ex2
 var inputElement = document.querySelector('#searchUser');
 var buttonElement = document.querySelector('button');
+var listElement = document.querySelector('ul');
 
 const urlgetUserFromGitHub = 'https://api.github.com/users/';
 
 buttonElement.onclick = searchRepositoryUser;
 
 function renderRepositories(repos) {
-    var listElement = document.querySelector('ul');
+    listElement.innerHTML = "";
     for (repo of repos) {
         var item = document.createElement('li');
         var nameRepo = document.createTextNode(repo.name);
-        item.appendChild(nameRepo); 
+        item.appendChild(nameRepo);
         listElement.appendChild(item);
     }
 }
 
+function renderLoading() {
+    listElement.innerHTML = '';
+    var item = document.createElement('li');
+    var text = document.createTextNode('Carregando...');
+    item.appendChild(text);
+    listElement.appendChild(item);
+}
+
+function renderError() {
+    listElement.innerHTML = "";
+    var item = document.createElement('li');
+    var text = document.createTextNode('Erro ao buscar repositório...');
+    item.style.color = "#F00";
+    item.appendChild(text);
+    listElement.appendChild(item);
+}
+
 function searchRepositoryUser() {
+    renderLoading();
     var user = inputElement.value;
     if (user !== "") {
         axios.get(urlgetUserFromGitHub + user + '/repos')
@@ -41,7 +60,7 @@ function searchRepositoryUser() {
                 renderRepositories(response.data);
             })
             .catch(function (error) {
-                console.warn(error);
+                renderError();
             });
     }
 }
