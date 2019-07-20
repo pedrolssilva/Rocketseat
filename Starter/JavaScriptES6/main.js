e50a776e95773d59b5e974da84d2047823d86a19
pedrolssilva <pedrolssilva@hotmail.com>
2019-07-20 15:23:46 -0300

    learning about variables, arrays operations and arrow functions

--- Starter/JavaScriptES6/main.js
+++ Starter/JavaScriptES6/main.js
@@ -1,36 +1,56 @@
-// class List {
-//     constructor() {
-//         this.data = [];
-//     }
-
-//     add(data) {
-//         this.data.push(data);
-//         console.log(this.data);
-//     }
-// }
-
-// class TodoList extends List {
-//     constructor() {
-//         super();
-
-//         this.usuario = 'Diego';
-//     }
-
-//     mostraUsuario() {
-//         console.log(this.usuario);
-//     }
-// }
-
-// var MinhaLista = new TodoList();
-
-// document.getElementById('novotodo').onclick = function() {
-//     MinhaLista.add('Novo todo');
-// }
-
-// MinhaLista.mostraUsuario();
-
 class Matematica {
     static soma(a, b) {
         return a + b;
     }
 }
+
+//Keywords
+const usuario = { nome: 'Diego' }; //Const can't be reassign;
+usuario.nome = 'Cleiton';
+console.log(usuario);
+
+function teste(x) {
+    let y = 2;
+    if (x > 5) {
+        console.log(x, y);
+    }
+}
+console.log(teste(18));
+
+
+//operaçoes em arrays
+{
+    const arr = [1, 3, 4, 5, 8, 9];
+
+    const newArr = arr.map(function (item, index) { //map permite manipular array
+        return item + index;
+    })
+
+    console.log(newArr);
+
+    const sum = arr.reduce(function (total, next) {
+        return total + next;
+    });
+    console.log(sum);
+
+    const filter = arr.filter(function (item) {
+        return item % 2 === 0;
+    });
+
+    const find = arr.find(function (item) {
+        return item === 4;
+    });
+    console.log(find);
+}
+
+//ArrowFunctions
+const arr = [1,3,4,5,6];
+const newArr = arr.map(item => item *2);
+console.log(newArr);
+
+// const teste = () => {
+//     return 'teste';
+// }
+
+const teste = () => ({nome: 'Diego'}); // retorna um objeto
+console.log(teste());
