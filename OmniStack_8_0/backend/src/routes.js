64d807af45ee916dbdc2e9c0cfaee64a8eba4e37
pedrolssilva <pedrolssilva@hotmail.com>
2019-08-06 23:05:55 -0300

    finalizando api backend com node para consultar users com filtro, dar like, dar dislike e cadastrar novo usuario

--- OmniStack_8_0/backend/src/routes.js
+++ OmniStack_8_0/backend/src/routes.js
@@ -1,17 +1,14 @@
 const express = require('express');
-
 const routes = express.Router();
+const DevController = require ('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 // GET, POST, PUT, DELETE => Utilizando Rest
 //When browser make route request always is get method
 
-routes.get('/', (req, res) => {
-
-    req.query.name
-    return res.json({ message: `Olá ${req.query.name}` });
-});
-
-routes.post('/devs', (req, res) => {
-    return res.json({ ok: true });
-})
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
