0c8921e6e10d7df00a1f3f74aa42505770fdd56f
pedrolssilva <pedrolssilva@hotmail.com>
2019-08-15 22:39:31 -0300

    finalizado app com like e dislike pendente apenas match em real time

--- OmniStack_8_0/tindev/src/pages/Login.js
+++ OmniStack_8_0/tindev/src/pages/Login.js
@@ -1,41 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
-import {KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity} from 'react-native';
+import { KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 import logo from '../assets/logo.png';
 import api from '../services/api';
 
-export default function Login({navigation}){
+export default function Login({ navigation }) {
     const [user, setUser] = useState('');
-    useEffect(() =>{
-        AsyncStorage.getItem('user').then(user =>{
-            if(user){
-                navigation.navigate('Main', {user})
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if (user) {
+                navigation.navigate('Main', { user })
             }
         })
     }, []);
 
-    async function handleLogin(){
-        const response = await api.post('/devs', {username: user});
-        const {_id} = response.data;
+    async function handleLogin() {
+        const response = await api.post('/devs', { username: user });
+        const { _id } = response.data;
         console.log(_id);
-        await AsyncStorage.setItem('user',_id);
-        navigation.navigate('Main', _id);
+        await AsyncStorage.setItem('user', _id);
+        navigation.navigate('Main', { user: _id });
     }
     return (
-        <KeyboardAvoidingView 
-        behavior="padding"
-        enabled={Platform.OS === 'ios'}
-        style={styles.container}
+        <KeyboardAvoidingView
+            behavior="padding"
+            enabled={Platform.OS === 'ios'}
+            style={styles.container}
         >
             <Image source={logo}></Image>
-            <TextInput 
-            autoCapitalize = 'none'
-            autoCorrect = {false}
-            placeholder="Digite seu usuário do GitHub"
-            placeholderTextColor="#999"
-            style={styles.input}
-            value={user}
-            onChangeText={setUser}
+            <TextInput
+                autoCapitalize='none'
+                autoCorrect={false}
+                placeholder="Digite seu usuário do GitHub"
+                placeholderTextColor="#999"
+                style={styles.input}
+                value={user}
+                onChangeText={setUser}
             />
             <TouchableOpacity onPress={handleLogin} style={styles.button}>
                 <Text style={styles.buttonText}>Enviar</Text>
@@ -44,7 +44,7 @@ export default function Login({navigation}){
     )
 };
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         backgroundColor: '#f5f5f5',
         justifyContent: 'center',
@@ -52,29 +52,29 @@ const styles = StyleSheet.create({
         padding: 30
     },
 
-    input:{
+    input: {
         height: 46,
         alignSelf: 'stretch',
         backgroundColor: '#fff',
         borderWidth: 1,
         borderColor: '#ddd',
         borderRadius: 4,
-        marginTop: 20, 
+        marginTop: 20,
         paddingHorizontal: 15,
     },
 
-    button:{
+    button: {
         height: 46,
-        alignSelf:'stretch',
+        alignSelf: 'stretch',
         backgroundColor: '#df4723',
         borderRadius: 4,
-        marginTop: 10, 
+        marginTop: 10,
         justifyContent: 'center',
         alignItems: 'center',
     },
 
-    buttonText:{
-        color:'#fff',
+    buttonText: {
+        color: '#fff',
         fontWeight: 'bold',
         fontSize: 16,
     },
