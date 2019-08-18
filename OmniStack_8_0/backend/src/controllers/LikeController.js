47ee5a6e3b8343cebdfdf1dd86d0ff3ff7981140
pedrolssilva <pedrolssilva@hotmail.com>
2019-08-18 17:50:18 -0300

    implemented socket.io and socket.io-client in server and front to realtime notifications, implemented midlleware to pass params to LikeController, styled component match in front and add match component in Main.js

--- OmniStack_8_0/backend/src/controllers/LikeController.js
+++ OmniStack_8_0/backend/src/controllers/LikeController.js
@@ -1,24 +1,33 @@
 const Dev = require('../models/Dev');
 
 module.exports = {
-    async store(req, res){
-        const {devId} = req.params;
-        const {user} = req.headers;
+    async store(req, res) {
+        const { devId } = req.params;
+        const { user } = req.headers;
 
         const loggedDev = await Dev.findById(user);
         const targetDev = await Dev.findById(devId);
 
-        if(!targetDev){
-            return res.status(400).json({error: 'Dev not exists'});
+        if (!targetDev) {
+            return res.status(400).json({ error: 'Dev not exists' });
         }
 
-        if(targetDev.likes.includes(user)){
-            console.log('DEU MATCH');
+        if (targetDev.likes.includes(user)) {
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if (loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
 
         loggedDev.likes.push(targetDev._id);
-       
+
         await loggedDev.save();
 
         return res.json(loggedDev);
